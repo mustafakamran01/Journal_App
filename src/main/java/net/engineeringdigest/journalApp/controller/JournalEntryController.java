4763929795a79ac67dcf8c81082986b90aabb1eb
mustafakamran01 <kamran000mustafa@gmail.com>
2026-03-25 20:30:07 +0530

    Create endpoints

--- src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -10,37 +10,6 @@ import java.util.*;
 @RequestMapping("/journal")
 public class JournalEntryController {
 
-//    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
-
-//    @GetMapping("/allEntries")
-//    public List<JournalEntry> getAllEntries() {
-//        return new ArrayList<>(journalEntries.values());
-//    }
-//
-//    @PostMapping("/createEntry")
-//    public boolean createEntry(@RequestBody JournalEntry myEntry) {
-//        journalEntries.put(myEntry.getId(), myEntry);
-//        return true;
-//    }
-//
-//    @GetMapping("/singleEntry/{myId}")
-//    public JournalEntry getEntryById(@PathVariable Long myId) {
-//        return journalEntries.get(myId);
-//    }
-//
-//    @DeleteMapping("/delete/{id}")
-//    public boolean deleteEntryById(@PathVariable Long id) {
-//        journalEntries.remove(id);
-//        return true;
-//    }
-//
-//    @PutMapping("/update/{id}")
-//    public JournalEntry updateEntryById(@PathVariable Long id, @RequestBody JournalEntry entry) {
-//        JournalEntry oldEntry = journalEntries.get(id);
-//        journalEntries.put(id, entry);
-//        return oldEntry;
-//    }
-
     private Map<Long, JournalEntry> journalEntries = new HashMap<>();
 
     @GetMapping("/getAll")
@@ -72,7 +41,4 @@ public class JournalEntryController {
         journalEntries.remove(id);
         return true;
     }
-
-
-
 }
