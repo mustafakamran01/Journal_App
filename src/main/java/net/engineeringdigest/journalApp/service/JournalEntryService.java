565138fabc57cd293ad38acba9ffe5175bb7c0b3
mustafakamran01 <kamran000mustafa@gmail.com>
2026-03-26 15:21:30 +0530

    Create services and controller

--- src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
@@ -15,22 +15,6 @@ public class JournalEntryService {
     @Autowired
     private JournalEntryRepository journalEntryRepository;
 
-//    public void saveEntry(JournalEntry journalEntry) {
-//        journalEntryRepository.save(journalEntry);
-//    }
-//
-//    public List<JournalEntry> getAllEntry() {
-//        return journalEntryRepository.findAll();
-//    }
-//
-//    public Optional<JournalEntry> findById(ObjectId id) {
-//        return journalEntryRepository.findById(id);
-//    }
-//
-//    public void deleteById(ObjectId id) {
-//        journalEntryRepository.deleteById(id);
-//    }
-
     public List<JournalEntry> getAllEntry() {
         return journalEntryRepository.findAll();
     }
