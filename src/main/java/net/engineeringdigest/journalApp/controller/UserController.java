8803b8bd7ef7e71fb6c68eeec1d57801b66f17d9
mustafakamran01 <kamran000mustafa@gmail.com>
2026-03-28 20:54:47 +0530

    Create services and controller

--- src/main/java/net/engineeringdigest/journalApp/controller/UserController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/UserController.java
@@ -4,9 +4,13 @@ import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.service.UserService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -15,26 +19,52 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping
-    public List<User> getAllUsers() {
-        return userService.getAll();
+    @GetMapping("/getAll")
+    public ResponseEntity<?> getAllUsers() {
+        Map<String, Object> response = new HashMap<>();
+        List<User> allUsers = userService.getAll();
+        if (allUsers == null || allUsers.isEmpty()) {
+            response.put("recordFound", false);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+        response.put("recordFound", true);
+        return ResponseEntity.status(HttpStatus.FOUND).body(response);
     }
 
-    @PostMapping
-    public void createUser(@RequestBody User user) {
-        userService.createUser(user);
-    }
-
-    @PutMapping("/update/{id}")
-    public void updateUser(@RequestBody User user, @PathVariable ObjectId id) {
-        User userInDB = userService.findById(id);
-        if (userInDB != null) {
-            userInDB.setUserName(user.getUserName());
-            userInDB.setPassword(user.getPassword());
-            userService.createUser(userInDB);
+    @PostMapping("/createUser")
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            userService.createUser(user);
+            response.put("recordCreated", true);
+            response.put("record", user);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (Exception e) {
+            response.put("recordCreated", false);
+            response.put("errorMessage", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
     }
 
-
-
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable ObjectId id) {
+        Map<String, Object> response = new HashMap<>();
+        User userInDB = userService.findById(id);
+        if (userInDB != null) {
+            try{
+                userInDB.setUserName(user.getUserName());
+                userInDB.setPassword(user.getPassword());
+                userService.createUser(userInDB);
+                response.put("recordUpdated", true);
+                response.put("updatedRecord", userInDB);
+                return ResponseEntity.status(HttpStatus.CREATED).body(response);
+            } catch (Exception e) {
+                response.put("recordUpdated", false);
+                response.put("errorMessage", e.getMessage());
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+            }
+        }
+        response.put("recordFound", false);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
 }
